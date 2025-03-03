1d782e730cc6d64a7619fe10bd33adacd4e5ea91
cychann <skcbdlhc129@naver.com>
2025-03-03 23:42:28 +0900

    [chore]: React-Router + Vite 플러그인 초기 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
+import { reactRouter } from '@react-router/dev/vite';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()],
   server: {
     port: 3000,
   },
