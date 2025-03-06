21aa799ff172cc2f478c66018b57ab0269c1d25f
cychann <skcbdlhc129@naver.com>
2025-03-06 15:12:43 +0900

    [fix]: index css import 경로 수정

--- frontend/src/entry.client.tsx
+++ frontend/src/entry.client.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HydratedRouter } from 'react-router/dom';
-import './index.css';
+import '@/styles/index.css';
 
 ReactDOM.hydrateRoot(
   document,
