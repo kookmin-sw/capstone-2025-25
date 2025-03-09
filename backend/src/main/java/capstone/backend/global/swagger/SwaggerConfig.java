a0bb303f6968655c9ac068cdaf33269080459dd7
ryugeon <sksmsfbrjs@naver.com>
2025-03-10 01:38:48 +0900

    [chore]: Swagger Configuration 코드 간결화 리팩토링

--- backend/src/main/java/capstone/backend/global/swagger/SwaggerConfig.java
+++ backend/src/main/java/capstone/backend/global/swagger/SwaggerConfig.java
@@ -1,37 +1,36 @@
 package capstone.backend.global.swagger;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Value("${baseUrl.server}")
-    private String serverUrl;
-
     @Bean
     public OpenAPI openAPI() {
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList("JWT");
-
-        Components components = new Components()
-                .addSecuritySchemes("JWT", new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")
-                );
 
         return new OpenAPI()
-                .info(new Info()
-                        .title("App Name")
-                        .description("성인 ADHD 환자를 돕기 위한 솔루션 서비스 - 'App Name' API Docs.")
-                        .version("v1"))
-                .addSecurityItem(securityRequirement)
-                .components(components);
+                .components(new Components().addSecuritySchemes("JWT", securityScheme()))
+                .info(info());
     }
-}
+
+    private Info info() {
+
+        return new Info()
+                .title("App Name")
+                .description("성인 ADHD 환자들을 위한 솔루션 서비스 - App Name")
+                .version("1.0");
+    }
+
+    private SecurityScheme securityScheme() {
+
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }}
