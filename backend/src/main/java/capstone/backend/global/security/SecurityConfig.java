9ad0562f7cc6a0eacd35b3be32a7b3ba819c439f
ryugeon <sksmsfbrjs@naver.com>
2025-03-10 01:44:20 +0900

    [feat]: 메서드 수준 보안 활성화를 위한 @EnableMethodSecurity 어노테이션 삽입

--- backend/src/main/java/capstone/backend/global/security/SecurityConfig.java
+++ backend/src/main/java/capstone/backend/global/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import capstone.backend.global.security.oauth2.service.OAuth2UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -22,6 +23,7 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
