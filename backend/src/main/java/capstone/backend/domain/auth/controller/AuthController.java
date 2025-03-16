663bcc8059a555423ff6fee5f56a68707325b1f0
ryugeon <sksmsfbrjs@naver.com>
2025-03-17 01:11:43 +0900

    feat: Refresh Token 도메인 생성 코드 추가

---
+++ backend/src/main/java/capstone/backend/domain/auth/controller/AuthController.java
@@ -0,0 +1,27 @@
+package capstone.backend.domain.auth.controller;
+
+import capstone.backend.domain.auth.dto.request.RefreshAccessTokenRequest;
+import capstone.backend.domain.auth.dto.response.RefreshAccessTokenResponse;
+import capstone.backend.domain.auth.service.AuthService;
+import capstone.backend.global.api.dto.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/refresh-token")
+    @Operation(summary = "Access Token 재발급")
+    public ApiResponse<RefreshAccessTokenResponse> refreshToken(
+            @RequestBody @Valid RefreshAccessTokenRequest request
+    ) {
+        return ApiResponse.ok(authService.refreshAccessToken(request));
+    }
+
+}
---
+++ backend/src/main/java/capstone/backend/domain/auth/service/AuthService.java
@@ -0,0 +1,29 @@
+package capstone.backend.domain.auth.service;
+
+import capstone.backend.domain.auth.dto.request.RefreshAccessTokenRequest;
+import capstone.backend.domain.auth.dto.response.RefreshAccessTokenResponse;
+import capstone.backend.domain.auth.exception.RefreshTokenExpiredException;
+import capstone.backend.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final JwtProvider jwtProvider;
+
+    public RefreshAccessTokenResponse refreshAccessToken(RefreshAccessTokenRequest request) {
+        Optional<String> newAccessToken = jwtProvider.refreshAccessToken(request.token());
+
+        if (newAccessToken.isPresent()) {
+            return new RefreshAccessTokenResponse(newAccessToken.get());
+        } else {
+            throw new RefreshTokenExpiredException();
+        }
+    }
+}
