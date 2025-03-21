d2b72c68bcce34396b3a6ab412855df1de6d1100
ryugeon <sksmsfbrjs@naver.com>
2025-03-21 22:43:18 +0900

    fix: 프론트에서 전송해준 쿠키에서 토큰을 파싱하는 로직으로 수정

--- backend/src/main/java/capstone/backend/domain/auth/service/AuthService.java
+++ backend/src/main/java/capstone/backend/domain/auth/service/AuthService.java
@@ -1,6 +1,5 @@
 package capstone.backend.domain.auth.service;
 
-import capstone.backend.domain.auth.dto.request.RefreshAccessTokenRequest;
 import capstone.backend.domain.auth.dto.response.RefreshAccessTokenResponse;
 import capstone.backend.domain.auth.exception.RefreshTokenExpiredException;
 import capstone.backend.global.security.jwt.JwtProvider;
@@ -13,8 +12,8 @@ public class AuthService {
 
     private final JwtProvider jwtProvider;
 
-    public RefreshAccessTokenResponse refreshAccessToken(RefreshAccessTokenRequest request) {
-        String newAccessToken = jwtProvider.refreshAccessToken(request.token());
+    public RefreshAccessTokenResponse refreshAccessToken(String refreshToken) {
+        String newAccessToken = jwtProvider.refreshAccessToken(refreshToken);
 
         if (newAccessToken != null) {
             return new RefreshAccessTokenResponse(newAccessToken);
