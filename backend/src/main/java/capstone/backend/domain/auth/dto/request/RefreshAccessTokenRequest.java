4eb084fb24a7b8f31994580e3dc667949f31de98
ryugeon <sksmsfbrjs@naver.com>
2025-03-21 19:10:51 +0900

    refactor: @Builder 삭제

--- backend/src/main/java/capstone/backend/domain/auth/dto/request/RefreshAccessTokenRequest.java
+++ backend/src/main/java/capstone/backend/domain/auth/dto/request/RefreshAccessTokenRequest.java
@@ -1,9 +1,7 @@
 package capstone.backend.domain.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
 
-@Builder
 public record RefreshAccessTokenRequest (
     @NotBlank
     String token
