a8a78d89d6c7e06c12ba4fef561a978a4091735f
cychann <skcbdlhc129@naver.com>
2025-03-26 19:32:10 +0900

    [style]: dragIcon 수정

--- frontend/src/components/reactFlow/nodes/ui/RootNode.tsx
+++ frontend/src/components/reactFlow/nodes/ui/RootNode.tsx
@@ -1,6 +1,5 @@
 import { NodeProps } from '@xyflow/react';
-import { ListCheck } from 'lucide-react';
-import DragIcon from '@/components/reactFlow/nodes/ui/DragIcon';
+import { GripVertical, ListCheck } from 'lucide-react';
 import { RootNodeType } from '@/types/mindMap';
 import NodeHandles from '@/components/reactFlow/nodes/ui/NodeHandles';
 
@@ -12,12 +11,10 @@ export default function RootNode({ data, selected }: NodeProps<RootNodeType>) {
       }`}
     >
       <div className="dragHandle absolute top-2 left-2 z-20 cursor-grab opacity-0 group-hover:opacity-100 transition-opacity duration-200">
-        <DragIcon />
+        <GripVertical size={20} color="#B9B9B7" />
       </div>
 
-      <div className="z-10 absolute -top-[25px] left-1/2 transform -translate-x-1/2 w-[50px] h-[50px] bg-root-icon rounded-full border border-black flex items-center justify-center">
-        <span className="text-lg">✅</span>
-      </div>
+      <div className="z-10 absolute -top-[25px] left-1/2 transform -translate-x-1/2 w-[50px] h-[50px] bg-root-icon rounded-full border border-black flex items-center justify-center" />
 
       <ListCheck size={25} color="#D8D8D8" className="absolute top-4 right-4" />
 
