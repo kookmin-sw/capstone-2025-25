7a2c2e8e0d3fc7fff6e6005e9a048449cca7ce98
daun-up <awhekdns@kookmin.ac.kr>
2025-03-31 19:50:00 +0900

    feat: react-router 관련 업데이트 (자동 생성)

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router';
+import PriorityMatrix from '@/components/PriorityMatrix/PriorityMatrix.tsx';
 
 export default function DefaultLayout() {
   return (
     <div>
       <header>Default header</header>
       <Outlet />
+      <PriorityMatrix />
     </div>
   );
 }
