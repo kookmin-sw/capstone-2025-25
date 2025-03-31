929792454e37a4c343091d83a496cd004c52acad
daun-up <awhekdns@kookmin.ac.kr>
2025-03-31 17:30:39 +0900

    fix: sidebar 폴더 수정으로 인한 경로 수정

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router';
-import Sidebar from '@/components/ui/Sidebar.tsx';
+import Sidebar from '@/components/ui/sidebar/Sidebar.tsx';
 
 export default function DefaultLayout() {
   return (
