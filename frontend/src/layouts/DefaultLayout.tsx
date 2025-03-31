193fe6b1bbbebef69677a1ca8782ccca27e41cc1
daun-up <awhekdns@kookmin.ac.kr>
2025-03-31 14:36:43 +0900

    chore: 사이드바 svg 관련 해결 중...

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router';
+import Sidebar from '@/components/ui/Sidebar.tsx';
 
 export default function DefaultLayout() {
   return (
     <div>
       <header>Default header</header>
+        <Sidebar />
       <Outlet />
     </div>
   );
