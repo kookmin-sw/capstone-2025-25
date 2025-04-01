4898f7d52dd48e656376a0c56e009223c86e293a
daun-up <awhekdns@kookmin.ac.kr>
2025-04-01 15:02:50 +0900

    fix: outlet 요소 나오게 처리

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router';
-import Sidebar from '@/components/ui/sidebar/Sidebar.tsx';
+import Sidebar from '@/components/ui/sidebar/Sidebar';
 
 export default function DefaultLayout() {
   return (
-    <div>
-      <header>Default header</header>
-        <Sidebar />
-      <Outlet />
+    <div className="flex h-screen">
+      <Sidebar />
+
+      <main className="flex-1 overflow-auto bg-gray-50">
+        <Outlet />
+      </main>
     </div>
   );
 }
