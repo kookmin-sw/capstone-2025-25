bde5c2ebf3c820a6c7da70a78ab35a057aad9176
Dohun Kim <dohun8758@naver.com>
2025-04-03 13:11:45 +0900

    feat: 유저에 해당하는 아이젠하워 작업(완료, 미완료) 메서드 구현

--- backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
@@ -1,9 +1,13 @@
 package capstone.backend.domain.eisenhower.repository;
 
 import capstone.backend.domain.eisenhower.entity.EisenhowerItem;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EisenhowerItemRepository extends JpaRepository<EisenhowerItem, Long> {
+    List<EisenhowerItem> findAllByMemberIdAndIsCompletedFalse(Long memberId);
+
+    List<EisenhowerItem> findAllByMemberIdAndIsCompletedTrue(Long memberId);
 }
