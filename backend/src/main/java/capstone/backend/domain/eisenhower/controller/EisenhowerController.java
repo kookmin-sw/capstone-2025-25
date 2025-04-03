39efa11156cd938752b0e546772d3f7bf4b34025
Dohun Kim <dohun8758@naver.com>
2025-04-03 16:09:42 +0900

    feat: 아이젠하워 작업 수정 API 구현

--- backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
@@ -1,5 +1,6 @@
 package capstone.backend.domain.eisenhower.controller;
 
+import capstone.backend.domain.eisenhower.dto.response.EisenhowerUpdateRequest;
 import capstone.backend.domain.eisenhower.dto.request.EisenhowerItemCreateRequest;
 import capstone.backend.domain.eisenhower.dto.response.EisenhowerItemResponse;
 import capstone.backend.domain.eisenhower.service.EisenhowerService;
@@ -11,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -42,5 +45,14 @@ public class EisenhowerController {
             return ApiResponse.ok(eisenhowerService.getItemsCompleted(customOAuth2User.getMemberId()));
         }
         return ApiResponse.ok(eisenhowerService.getItemsNotCompleted(customOAuth2User.getMemberId()));
+    }
+
+    @PatchMapping("/{itemId}")
+    public ApiResponse<EisenhowerItemResponse> updateItem(
+            @RequestBody @Valid EisenhowerUpdateRequest request,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+            @PathVariable Long itemId
+    ) {
+        return ApiResponse.ok(eisenhowerService.updateItem(customOAuth2User.getMemberId(), itemId, request));
     }
 }
