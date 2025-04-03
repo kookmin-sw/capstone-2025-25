14fe5fe3e9006fa93d4bd5ee148d100ca66c6419
Dohun Kim <dohun8758@naver.com>
2025-04-03 16:09:21 +0900

    feat: member_id와 category_id로 EisenhowerCategory 조회 메서드 추가

--- backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerCategoryRepository.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerCategoryRepository.java
@@ -1,9 +1,11 @@
 package capstone.backend.domain.eisenhower.repository;
 
 import capstone.backend.domain.eisenhower.entity.EisenhowerCategory;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EisenhowerCategoryRepository extends JpaRepository<EisenhowerCategory, Long> {
+    Optional<EisenhowerCategory> findByIdAndMemberId(Long categoryId, Long memberId);
 }
