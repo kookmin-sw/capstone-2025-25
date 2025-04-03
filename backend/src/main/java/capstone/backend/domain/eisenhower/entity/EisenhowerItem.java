75d30f98df768c94e86425b7bdf37df78e58a88a
Dohun Kim <dohun8758@naver.com>
2025-04-03 10:24:00 +0900

    feat: EisenhowerItem 엔티티 작성
    - 뽀모도로 필드는 뽀모도로 기능 개발 이후 적용 예정으로 주석 처리
    - 정적 팩터리 메서드 사용하여 객체 생성 후 반환

---
+++ backend/src/main/java/capstone/backend/domain/eisenhower/entity/EisenhowerItem.java
@@ -0,0 +1,89 @@
+package capstone.backend.domain.eisenhower.entity;
+
+import capstone.backend.domain.common.entity.TaskType;
+import capstone.backend.domain.eisenhower.dto.request.EisenhowerItemCreateRequest;
+import capstone.backend.domain.member.scheme.Member;
+import capstone.backend.domain.mindmap.entity.MindMap;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class EisenhowerItem {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "eisenhower_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false)
+    private String title;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private EisenhowerCategory category;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private EisenhowerQuadrant quadrant;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskType type;
+
+    @Column(nullable = false, name = "task_order", unique = true)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean isCompleted;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    private String memo;
+
+    private LocalDate dueDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    private MindMap mindMap;
+
+//    @OneToOne(fetch = FetchType.LAZY)
+//    private Pomodoro pomodoro;
+
+
+    public static EisenhowerItem from(EisenhowerItemCreateRequest request, Member member, EisenhowerCategory category) {
+        return EisenhowerItem.builder()
+                .member(member)
+                .title(request.title())
+                .category(category)
+                .dueDate(request.dueDate())
+                .quadrant(request.quadrant())
+                .type(request.type())
+                .order(request.order())
+                .isCompleted(false)
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
