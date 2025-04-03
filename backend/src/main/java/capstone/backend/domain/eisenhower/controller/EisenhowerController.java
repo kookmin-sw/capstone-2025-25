16e68a6523db5a86618056966c05f5d498fe0929
Dohun Kim <dohun8758@naver.com>
2025-04-03 13:13:03 +0900

    feat: 아이젠하워 작업 리스트 조회 API 구현
    - completed를 param으로 받아 값에 따른 로직 수행
    - 추후 필터링 기능이 생길시에 param을 이용하여 수행할 예정

--- backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
@@ -6,12 +6,15 @@ import capstone.backend.domain.eisenhower.service.EisenhowerService;
 import capstone.backend.global.api.dto.ApiResponse;
 import capstone.backend.global.security.oauth2.user.CustomOAuth2User;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,5 +31,16 @@ public class EisenhowerController {
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
         return ApiResponse.ok(eisenhowerService.createItem(request, customOAuth2User.getMemberId()));
+    }
+
+    @GetMapping
+    public ApiResponse<List<EisenhowerItemResponse>> getItems(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+            @RequestParam(required = false) Boolean completed
+    ) {
+        if(Boolean.TRUE.equals(completed)) {
+            return ApiResponse.ok(eisenhowerService.getItemsCompleted(customOAuth2User.getMemberId()));
+        }
+        return ApiResponse.ok(eisenhowerService.getItemsNotCompleted(customOAuth2User.getMemberId()));
     }
 }
