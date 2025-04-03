c32b9b62f18b29c074f68e4cfca267d2454ec31c
Dohun Kim <dohun8758@naver.com>
2025-04-03 12:24:59 +0900

    refactor: 아이젠하워 작업 생성 API ApiReponse 적용

--- backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/controller/EisenhowerController.java
@@ -3,14 +3,16 @@ package capstone.backend.domain.eisenhower.controller;
 import capstone.backend.domain.eisenhower.dto.request.EisenhowerItemCreateRequest;
 import capstone.backend.domain.eisenhower.dto.response.EisenhowerItemResponse;
 import capstone.backend.domain.eisenhower.service.EisenhowerService;
+import capstone.backend.global.api.dto.ApiResponse;
 import capstone.backend.global.security.oauth2.user.CustomOAuth2User;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/eisenhower")
@@ -21,11 +23,10 @@ public class EisenhowerController {
     private final EisenhowerService eisenhowerService;
 
     @PostMapping
-    public ResponseEntity<EisenhowerItemResponse> createItem(
+    public ApiResponse<EisenhowerItemResponse> createItem(
             @RequestBody @Valid EisenhowerItemCreateRequest request,
             @AuthenticationPrincipal CustomOAuth2User customOAuth2User
     ) {
-        EisenhowerItemResponse response = eisenhowerService.createItem(request, customOAuth2User.getMemberId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        return ApiResponse.ok(eisenhowerService.createItem(request, customOAuth2User.getMemberId()));
     }
 }
