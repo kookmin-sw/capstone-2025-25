cd3916bd4b4e4895bb60f5dfad95aea8ffa88bf9
daun-up <awhekdns@kookmin.ac.kr>
2025-04-07 21:43:09 +0900

    fix: matrix 페이지 수정

--- frontend/src/pages/matrix.tsx
+++ frontend/src/pages/matrix.tsx
@@ -1,10 +1,8 @@
 import PriorityMatrix from '@/components/PriorityMatrix/PriorityMatrix.tsx';
-import DefaultPriorityMatrix from '@/components/PriorityMatrix/DefaultPriorityMatrix.tsx';
 export default function MatrixPage() {
   return (
     <div className="flex flex-col gap-10">
       <PriorityMatrix />
-      <DefaultPriorityMatrix />
     </div>
   );
 }
