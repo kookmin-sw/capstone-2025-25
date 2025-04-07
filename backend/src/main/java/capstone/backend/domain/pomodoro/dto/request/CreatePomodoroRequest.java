16d2f85b749b453eb13bd3901ae3dc4159375f3e
ryugeon <sksmsfbrjs@naver.com>
2025-04-07 15:50:22 +0900

    rename: DTO 통합을 위한 이름 변경

--- backend/src/main/java/capstone/backend/domain/pomodoro/dto/request/CreatePomodoroRequest.java
+++ backend/src/main/java/capstone/backend/domain/pomodoro/dto/request/CreatePomodoroRequest.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.Pattern;
 
 import java.util.List;
 
-public record UnlinkedPomodoroRequest(
+public record CreatePomodoroRequest(
         @NotBlank(message = "총 시간을 입력해주세요.")
         @Pattern(
                 regexp = "^(?!00:00$)([0-1]?[0-9]|2[0-3]):[0-5][0-9]$",
