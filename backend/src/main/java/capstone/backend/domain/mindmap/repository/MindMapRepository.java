67d051af22300a834b649004063ba4275302842b
judyzero <qozmcll@kookmin.ac.kr>
2025-04-08 15:59:40 +0900

    chore: 불필요한 @Repository 제거

--- backend/src/main/java/capstone/backend/domain/mindmap/repository/MindMapRepository.java
+++ backend/src/main/java/capstone/backend/domain/mindmap/repository/MindMapRepository.java
@@ -6,12 +6,10 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MindMapRepository extends JpaRepository<MindMap, Long> {
     Optional<MindMap> findByIdAndMemberId(Long id, Long memberId);
-    boolean existsByIdAndMemberId(Long mindMapId, Long memberId);
+
     @Query("""
             SELECT m, e
             FROM MindMap m
