5049752ca9a786aded863abe1ebb1ded7fedf23c
judyzero <qozmcll@kookmin.ac.kr>
2025-04-08 15:54:18 +0900

    docs: 필드에 @Schema 설명 추가

--- backend/src/main/java/capstone/backend/domain/mindmap/dto/request/MindMapRequest.java
+++ backend/src/main/java/capstone/backend/domain/mindmap/dto/request/MindMapRequest.java
@@ -3,14 +3,27 @@ package capstone.backend.domain.mindmap.dto.request;
 import capstone.backend.domain.mindmap.entity.Edge;
 import capstone.backend.domain.mindmap.entity.MindMapType;
 import capstone.backend.domain.mindmap.entity.Node;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
 public record MindMapRequest(
+    @Schema(description = "아이젠하워 아이디")
     Long eisenhowerId,
-    @NotBlank String title,
-    @NotNull MindMapType type,
-    @NotNull List<Node> nodes,
+
+    @Schema(description = "마인드맵 제목")
+    @NotBlank(message = "제목을 입력해주세요.")
+    String title,
+
+    @Schema(description = "마인드맵 타입(TODO, THINKING)")
+    @NotNull(message = "타입을 입력해주세요.")
+    MindMapType type,
+
+    @Schema(description = "마인드맵 노드 정보")
+    @NotNull(message = "노드 정보를 입력해주세요.")
+    List<Node> nodes,
+
+    @Schema(description = "마인드맵 엣지 정보")
     List<Edge> edges
 ) {}
