8901afde1578c5e8a824376df2f4604ee438584a
Dohun Kim <dohun8758@naver.com>
2025-04-10 00:16:11 +0900

    chore: 사용하지 않는 코드 삭제

--- backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
@@ -1,17 +1,12 @@
 package capstone.backend.domain.eisenhower.repository;
 
 import capstone.backend.domain.eisenhower.entity.EisenhowerItem;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EisenhowerItemRepository extends JpaRepository<EisenhowerItem, Long>, EisenhowerItemRepositoryCustom {
-    List<EisenhowerItem> findAllByMemberIdAndIsCompletedFalse(Long memberId);
-
-    List<EisenhowerItem> findAllByMemberIdAndIsCompletedTrue(Long memberId);
-
     Optional<EisenhowerItem> findByIdAndMemberId(Long itemId, Long memberId);
 
     Page<EisenhowerItem> findByMemberIdAndTitleContaining(Long memberId, String keyword, Pageable pageable);
