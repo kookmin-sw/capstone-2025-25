c88ffa21d80a9fbb987a66448ebc46edc2555a9e
이보현 <84188904+250b@users.noreply.github.com>
2025-04-10 06:16:16 +0900

    fix: 삭제 뽀모도로 props 변경

--- frontend/src/components/ui/Modal/DeletePomodoro.tsx
+++ frontend/src/components/ui/Modal/DeletePomodoro.tsx
@@ -1,46 +1,43 @@
 import { Modal } from '@/components/common/Modal';
 import { Button } from '@/components/ui/button';
-import { Plus, Timer } from 'lucide-react';
-import { useState } from 'react';
+import {  Timer } from 'lucide-react';
 import { MultiSlider } from '@/components/ui/MultiSlider.tsx';
-import { PomodoroData } from '@/types/pomodoro';
+import { LinkedUnlinkedPomodoro } from '@/types/pomodoro';
+import {ReactNode} from "react";
 
 export default function DeletePomodoro({
-  pomodoroData,
+  trigger,
+  linkedUnlinkedPomodoro,
+  isOpen,
+  onOpenChange,
 }: {
-  pomodoroData: PomodoroData;
+  trigger: ReactNode;
+  linkedUnlinkedPomodoro: LinkedUnlinkedPomodoro;
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
 }) {
-  const [modalOpen, setModalOpen] = useState<boolean>(false);
-
-  const totalExecutedTime = pomodoroData.pomodoro.executedCycles.reduce(
+  const { pomodoro, eisenhower } = linkedUnlinkedPomodoro;
+  const totalExecutedTime = pomodoro.executedCycles.reduce(
     (sum, cycle) => sum + cycle.workDuration + (cycle.breakDuration ?? 0),
     0,
   );
 
   const deletePomodoro = () => {
     // 삭제 api 추가
-    setModalOpen(false);
+    console.log("deletePomodoro");
   };
 
   return (
     <Modal
-      trigger={<Plus size={24} className="cursor-pointer" />}
-      isOpen={modalOpen}
+      trigger={trigger}
+      isOpen={isOpen}
       title="뽀모도로를 삭제할까요?"
       description={`삭제 후 새로운 뽀모도로를 실행할 수 있어요`}
-      onOpenChange={(open) => {
-        setModalOpen(!modalOpen);
-      }}
+      onOpenChange={onOpenChange}
       footer={
         <div className="w-full flex justify-end">
           <div className="flex w-full justify-between gap-4">
-            <Button
-              variant="white"
-              className="px-8 flex-1"
-              onClick={() => {
-                setModalOpen(false);
-              }}
-            >
+            <Button variant="white" className="px-8 flex-1" onClick={()=>{onOpenChange(false)}}>
               취소하기
             </Button>
             <Button className="px-8 w-full flex-1" onClick={deletePomodoro}>
@@ -52,7 +49,7 @@ export default function DeletePomodoro({
     >
       <>
         <div className="flex flex-col gap-[33px]">
-          {pomodoroData.eisenhower ? (
+          {eisenhower ? (
             <div className="h-[153px] border-1"></div>
           ) : (
             <div className="flex px-4 py-4 border-1 border-[#E5E5E5] gap-2.5 rounded-[10px]">
@@ -66,7 +63,7 @@ export default function DeletePomodoro({
                 min={0}
                 max={totalExecutedTime}
                 step={1}
-                cycles={pomodoroData.pomodoro.executedCycles}
+                cycles={pomodoro.executedCycles}
                 readonly={true}
                 onValueChange={() => {}}
               />
