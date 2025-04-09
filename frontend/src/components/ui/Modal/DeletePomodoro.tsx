df238dc169b5905e4b536a98d85f08ad7e691d23
이보현 <84188904+250b@users.noreply.github.com>
2025-04-10 06:41:17 +0900

    fix: 불필요한 코드 제거

--- frontend/src/components/ui/Modal/DeletePomodoro.tsx
+++ frontend/src/components/ui/Modal/DeletePomodoro.tsx
@@ -65,7 +65,6 @@ export default function DeletePomodoro({
                 step={1}
                 cycles={pomodoro.executedCycles}
                 readonly={true}
-                onValueChange={() => {}}
               />
             </div>
           </div>
