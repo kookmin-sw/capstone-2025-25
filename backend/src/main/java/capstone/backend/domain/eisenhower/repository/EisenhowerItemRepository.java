1e993130badab79a3be11b1321575c823cef1519
Dohun Kim <dohun8758@naver.com>
2025-04-10 00:15:54 +0900

    feat: 아이젠하워 작업 검색 JPA 메서드 구현

--- backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerItemRepository.java
@@ -3,6 +3,8 @@ package capstone.backend.domain.eisenhower.repository;
 import capstone.backend.domain.eisenhower.entity.EisenhowerItem;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EisenhowerItemRepository extends JpaRepository<EisenhowerItem, Long>, EisenhowerItemRepositoryCustom {
@@ -11,4 +13,6 @@ public interface EisenhowerItemRepository extends JpaRepository<EisenhowerItem, 
     List<EisenhowerItem> findAllByMemberIdAndIsCompletedTrue(Long memberId);
 
     Optional<EisenhowerItem> findByIdAndMemberId(Long itemId, Long memberId);
+
+    Page<EisenhowerItem> findByMemberIdAndTitleContaining(Long memberId, String keyword, Pageable pageable);
 }
