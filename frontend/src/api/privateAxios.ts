6e4e35345662a18bc77773f0e9157c1a7c6990d0
daun-up <awhekdns@kookmin.ac.kr>
2025-04-10 14:53:43 +0900

    move: axios instance 이동

--- frontend/src/api/privateAxios.ts
+++ frontend/src/api/privateAxios.ts
@@ -1,12 +1,11 @@
 import axios from 'axios';
-import { useAuthStore } from '@/store/auth';
+import { useAuthStore } from '@/store/authStore.ts';
 
 const privateAxios = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
 
-// ✅ 요청 인터셉터에 토큰 자동 첨부
 privateAxios.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
   if (token) {
