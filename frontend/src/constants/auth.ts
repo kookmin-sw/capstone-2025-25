c1297ac6aa27cc5cdc3eb1aaa549b6a4d807b10c
daun-up <awhekdns@kookmin.ac.kr>
2025-04-11 13:25:19 +0900

    feat: constants 로 옮기기

--- frontend/src/constants/auth.ts
+++ frontend/src/constants/auth.ts
@@ -1,1 +1,4 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const GPT_BASE_URL =
+  import.meta.env.VITE_GPT_API_BASE_URL || 'http://3.36.179.66';
