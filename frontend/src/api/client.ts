db0bd9b20761a2dbb9e4810a0893cc9326d37610
daun-up <awhekdns@kookmin.ac.kr>
2025-04-11 11:59:47 +0900

    feat: refreshToken 로직 추가

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,30 +1,34 @@
 import axios from 'axios';
+import { ENDPOINTS } from '@/api/endpoints.ts';
 
-/* 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const GPT_BASE_URL =
+  import.meta.env.VITE_GPT_API_BASE_URL || 'http://3.36.179.66';
+
+/*
 백엔드 API 클라이언트
 TODO: 백엔드 API 주소 나오면 설정 예정
 */
 export const apiClient = axios.create({
-  baseURL: 'https://backend-server.com/',
+  baseURL: BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-/* 
+/*
 GPT API 클라이언트
-현재는 테스트 GPT API 서버인 http://3.36.179.66로 설정
 */
 export const gptClient = axios.create({
-  baseURL: 'http://3.36.179.66',
+  baseURL: GPT_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-/* 
+/*
 백엔드 API 요청 인터셉터 설정 (토큰 추가, 에러 처리 등)
-TODO: 추후에 토큰 로직 결정되면 그에 맞게 수정 필요. 
+TODO: 추후에 토큰 로직 결정되면 그에 맞게 수정 필요.
 */
 apiClient.interceptors.request.use(
   (config) => {
@@ -40,14 +44,39 @@ apiClient.interceptors.request.use(
 /*
 백엔드 API 응답 인터셉터 설정 (오류 처리 등)
 TODO: 추후 인증 관련 로직 결정되면 그에 맞게 수정 필요.
+refreshToken 처리 로직 추가
 */
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      window.location.href = '/login';
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const res = await axios.post(
+          `${BASE_URL}${ENDPOINTS.AUTH.REFRESH_TOKEN}`,
+          {},
+          {
+            withCredentials: true,
+          },
+        );
+
+        const newAccessToken = res.data?.content?.accessToken;
+        if (!newAccessToken) throw new Error('재발급 실패: accessToken 없음');
+
+        localStorage.setItem('token', newAccessToken);
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+
+        return apiClient(originalRequest);
+      } catch (reissueError) {
+        console.error('토큰 재발급 실패:', reissueError);
+        localStorage.removeItem('token');
+        window.location.href = '/login';
+      }
     }
+
     return Promise.reject(error);
   },
 );
