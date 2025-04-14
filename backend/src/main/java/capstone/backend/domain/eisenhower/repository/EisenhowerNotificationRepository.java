5cf2c26eb3f8a306220f97a66db71eca16a4ee59
Dohun Kim <dohun8758@naver.com>
2025-04-14 18:51:30 +0900

    refactor: 아이젠하워 알림 삭제 메서드 수정

--- backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerNotificationRepository.java
+++ backend/src/main/java/capstone/backend/domain/eisenhower/repository/EisenhowerNotificationRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EisenhowerNotificationRepository extends JpaRepository<EisenhowerNotification, Long> {
     void deleteByEisenhowerItemId(Long eisenhowerItemId);
-    void deleteByDueDateBefore(LocalDate date);
+    void deleteAllByDueDateBefore(LocalDate date);
     List<EisenhowerNotification> findAllByMemberIdOrderByDueDateDesc(Long memberId);
 }
