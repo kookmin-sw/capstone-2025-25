595572f5e36a4da5d8140034274ceb0ecc7489db
이보현 <84188904+250b@users.noreply.github.com>
2025-04-14 09:47:00 +0900

    style: 헤더 스타일링 수정

--- frontend/src/components/ui/header/Header.tsx
+++ frontend/src/components/ui/header/Header.tsx
@@ -2,18 +2,20 @@ import { Bell, Search, Settings } from 'lucide-react';
 
 export default function Header() {
   return (
-    <header className="px-10 py-5 h-[84px] flex items-center justify-between border-b">
+    <header className="px-7.5 py-2.5 h-[84px] flex items-center justify-between h-fit sticky top-0 w-full bg-white border-b border-b-[#E5E5E5]">
       <div className="flex-1 max-w-md">
         <div className="relative">
           <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5" />
           <input
             type="text"
             placeholder="검색어를 입력하세요"
-            className="w-full bg-[#f2f2f2] rounded-full py-[10px] pl-10 pr-4 text-sm focus:outline-none"
+            className="w-full bg-[#f2f2f2] rounded-full py-[7px] pl-10 pr-4 text-sm focus:outline-none"
           />
         </div>
       </div>
-      <div className="flex items-center gap-[30px]">
+      <div className="flex items-center gap-[20px]">
+        <div>
+        </div>
         <button className="cursor-pointer">
           <Settings className="w-5 h-5 text-gray-600" />
         </button>
