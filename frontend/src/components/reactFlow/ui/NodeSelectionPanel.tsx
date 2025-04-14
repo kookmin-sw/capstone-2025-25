0df64bf55a7be01cffc04412d0f05eef96a24a85
이보현 <84188904+250b@users.noreply.github.com>
2025-04-15 00:46:43 +0900

    style: node selection panel 수정

--- frontend/src/components/reactFlow/ui/NodeSelectionPanel.tsx
+++ frontend/src/components/reactFlow/ui/NodeSelectionPanel.tsx
@@ -54,12 +54,12 @@ export function NodeSelectionPanel() {
   };
 
   return (
-    <div className="w-[481px] fixed top-4 right-4 bg-white p-4 rounded-lg shadow-md border border-gray-200 z-[9999]">
+    <div className="w-[450px] fixed top-[65px] right-3 bg-white p-4 rounded-lg shadow-md border border-gray-200 z-[10]">
       <Collapsible open={isOpen} onOpenChange={setIsOpen} className="w-full">
-        <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center justify-between">
           <div>
-            <div className="font-bold mb-2">일정으로 생성할 노드 선택</div>
-            <p className="text-sm text-gray-500">
+            <div className="font-semibold text-[18px]">일정으로 생성할 노드 선택</div>
+            <p className="text-sm text-[#6E726E]">
               노드를 선택하여 하나의 일정으로 생성해 보세요
             </p>
           </div>
@@ -74,16 +74,15 @@ export function NodeSelectionPanel() {
 
         <CollapsibleContent className="transition-all data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
           {selectedNodes.length > 0 ? (
-            <ul className="flex-col space-y-2 mb-4">
+            <ul className="flex-col space-y-1 mt-4 mb-4">
               {selectedNodes.map((node) => (
                 <li
                   key={node.id}
-                  className="flex items-center justify-between border border-border-gray px-[20px] py-[15px] rounded-md"
+                  className="flex items-center justify-between border border-border-gray px-[15px] py-[10px] rounded-[7px]"
                 >
-                  <p>{node.data.summary || node.data.label}</p>
+                  <p className="text-[14px]">{node.data.summary || node.data.label}</p>
                   <X
-                    size={20}
-                    color="#6e726e"
+                    size={24}
                     className="cursor-pointer hover:bg-gray-100 rounded-full p-1 transition-colors"
                     onClick={() => removeSelectedNode(node.id)}
                   />
@@ -91,13 +90,14 @@ export function NodeSelectionPanel() {
               ))}
             </ul>
           ) : (
-            <div className="text-center py-4 mb-4 text-gray-500">
+            <div className="text-center p-4 mt-4 mb-4  text-[14px] text-[#6E726E]">
               선택된 노드가 없습니다. 노드를 클릭하여 선택해주세요.
             </div>
           )}
 
           <div className="flex items-center gap-4">
             <Button
+              size="sm"
               variant="outline"
               className="flex-1"
               onClick={handleCancelSelection}
@@ -105,9 +105,10 @@ export function NodeSelectionPanel() {
               취소하기
             </Button>
             <Button
+              size="sm"
               className="flex-1"
               onClick={handleCreateSchedule}
-              disabled={selectedNodes.length === 0 || isPending}
+              variant={selectedNodes.length === 0 || isPending ?'disabled':'black' }
             >
               {isPending ? (
                 <>
