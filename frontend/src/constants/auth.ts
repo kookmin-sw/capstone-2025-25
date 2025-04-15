5efe9bbaa9e2cc0912faf02862581e699d895904
cychann <skcbdlhc129@naver.com>
2025-04-15 17:59:58 +0900

    [fix]: GPT BASE URL 수정

--- frontend/src/constants/auth.ts
+++ frontend/src/constants/auth.ts
@@ -1,4 +1,3 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 export const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-export const GPT_BASE_URL =
-  import.meta.env.VITE_GPT_API_BASE_URL || 'http://3.36.179.66';
+export const GPT_BASE_URL = import.meta.env.VITE_GPT_API_BASE_URL;
