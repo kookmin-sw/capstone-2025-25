1e8bfc816bf13b1ec01985dd44b5e918ce4957b0
cychann <skcbdlhc129@naver.com>
2025-04-16 00:16:24 +0900

    [fix]: meta 데이터 정의 방식 수정

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -7,21 +7,37 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient();
 
+export function meta() {
+  return [
+    { title: 'AHZ' },
+    {
+      property: 'og:title',
+      content: 'AHZ',
+    },
+    {
+      property: 'og:description',
+      content:
+        '흩어진 생각을 정리하고, 일상 속 흐름을 바로 잡아주는 ADHD 맞춤형 서비스',
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'og:url',
+      content: 'https://www.ahz.co.kr/',
+    },
+    {
+      property: 'og:site_name',
+      content: 'AHZ',
+    },
+  ];
+}
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>AHZ</title>
-        <meta property="og:title" content="AHZ" />
-        <meta
-          property="og:description"
-          content="흩어진 생각을 정리하고, 일상 속 흐름을 바로 잡아주는 ADHD 맞춤형 서비스"
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.ahz.co.kr/" />
-        <meta property="og:site_name" content="AHZ" />
         <Meta />
         <Links />
       </head>
