cfae08be1521972c00835531797b36ebfa37724d
judyzero <qozmcll@kookmin.ac.kr>
2025-04-21 16:18:40 +0900

    refactor: 마인드맵 생성 요청 DTO에서 edges 필드 제거

--- backend/src/main/java/capstone/backend/domain/mindmap/dto/request/MindMapRequest.java
+++ backend/src/main/java/capstone/backend/domain/mindmap/dto/request/MindMapRequest.java
@@ -1,6 +1,5 @@
 package capstone.backend.domain.mindmap.dto.request;
 
-import capstone.backend.domain.mindmap.entity.Edge;
 import capstone.backend.domain.common.entity.TaskType;
 import capstone.backend.domain.mindmap.entity.Node;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -23,8 +22,5 @@ public record MindMapRequest(
 
     @Schema(description = "마인드맵 노드 정보")
     @NotEmpty(message = "노드 정보를 입력해주세요.")
-    List<Node> nodes,
-
-    @Schema(description = "마인드맵 엣지 정보")
-    List<Edge> edges
+    List<Node> nodes
 ) {}
