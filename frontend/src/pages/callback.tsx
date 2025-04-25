122d1477e909d419ea0d88d13c381489cf763ba0
daun-up <awhekdns@kookmin.ac.kr>
2025-04-25 14:01:13 +0900

    feat: authService 로 토큰 api 로직 분리

--- frontend/src/pages/callback.tsx
+++ frontend/src/pages/callback.tsx
@@ -1,9 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
 import { useMutation } from '@tanstack/react-query';
-import { apiClient } from '@/api/client.ts';
 import { authService } from '@/services/authService.ts';
-import { ENDPOINTS } from '@/api/endpoints.ts';
 
 export default function OAuthCallbackPage() {
   const navigate = useNavigate();
@@ -11,32 +9,8 @@ export default function OAuthCallbackPage() {
   const code = searchParams.get('code');
 
   const { mutate, isPending, error } = useMutation({
-    mutationFn: async (code: string) => {
-      const form = new URLSearchParams();
-      form.append('code', code);
-      const res = await apiClient.post(ENDPOINTS.AUTH.ACCESS_TOKEN, form, {
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        withCredentials: true,
-      });
-      return res.data;
-    },
-
-    onSuccess: (data) => {
-      console.log('백엔드 응답:', data);
-      const token =
-        data?.content?.accessToken ?? data?.accessToken ?? data?.token;
-
-      if (!token) {
-        console.error('accessToken 없음', data);
-        return;
-      }
-
-      authService.login(token);
-      navigate('/matrix');
-    },
-
+    mutationFn: (code: string) => authService.exchangeCodeForToken(code),
+    onSuccess: () => navigate('/matrix'),
     onError: (error) => {
       console.error('토큰 발급 실패:', error);
       navigate('/login');
@@ -49,7 +23,7 @@ export default function OAuthCallbackPage() {
     } else {
       mutate(code);
     }
-  }, [code, mutate]);
+  }, [code, mutate, navigate]);
 
   return (
     <p className="text-center mt-10">
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -1,3 +1,5 @@
+import { apiClient } from '@/api/client';
+import { ENDPOINTS } from '@/api/endpoints';
 import { useAuthStore } from '../store/authStore.ts';
 
 export const authService = {
@@ -7,7 +9,7 @@ export const authService = {
       return;
     }
 
-    document.cookie = `accessToken=${token}; path=/; max-age=3600`;
+    document.cookie = `accessToken=${token}; path=/;`;
 
     const setToken = useAuthStore.getState().setToken;
     setToken(token);
@@ -18,5 +20,30 @@ export const authService = {
 
     const setToken = useAuthStore.getState().setToken;
     setToken(null);
+  },
+
+  /** 로그인 백엔드 요청 후 response 로 받은 code 를 가지고 다시 재요청*/
+  exchangeCodeForToken: async (code: string) => {
+    const form = new URLSearchParams();
+    form.append('code', code);
+
+    const res = await apiClient.post(ENDPOINTS.AUTH.ACCESS_TOKEN, form, {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      withCredentials: true,
+    });
+
+    const token =
+      res.data?.content?.accessToken ??
+      res.data?.accessToken ??
+      res.data?.token;
+
+    if (!token) {
+      throw new Error('accessToken 없음');
+    }
+
+    authService.login(token);
+    return token;
   },
 };
