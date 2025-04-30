a05abf4e652874f6ada5d6a3cb49b5aebf16e2d0
daun-up <awhekdns@kookmin.ac.kr>
2025-04-30 10:17:35 +0900

    refactor: useExchangeCodeForToken 훅으로 분리

--- frontend/src/pages/callback.tsx
+++ frontend/src/pages/callback.tsx
@@ -1,29 +1,28 @@
 import { useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
-import { useMutation } from '@tanstack/react-query';
-import { authService } from '@/services/authService.ts';
+import useExchangeCodeForToken from '@/hooks/useExchangeCodeForToken.ts';
 
 export default function OAuthCallbackPage() {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const code = searchParams.get('code');
-
-  const { mutate, isPending, error } = useMutation({
-    mutationFn: (code: string) => authService.exchangeCodeForToken(code),
-    onSuccess: () => navigate('/matrix'),
-    onError: (error) => {
-      console.error('토큰 발급 실패:', error);
-      navigate('/login');
-    },
-  });
+  const { exchangeCodeForTokenMutation, isPending, error } =
+    useExchangeCodeForToken();
 
   useEffect(() => {
     if (!code) {
       navigate('/login');
-    } else {
-      mutate(code);
+      return;
     }
-  }, [code, mutate, navigate]);
+
+    exchangeCodeForTokenMutation(code, {
+      onSuccess: () => navigate('/matrix'),
+      onError: (error) => {
+        console.error('토큰 발급 실패:', error);
+        navigate('/login');
+      },
+    });
+  }, [code, exchangeCodeForTokenMutation, navigate]);
 
   return (
     <p className="text-center mt-10">
@@ -31,7 +30,8 @@ export default function OAuthCallbackPage() {
         ? `토큰 발급 실패: ${error.message}`
         : isPending
           ? '로그인 처리 중입니다...'
-          : ''}
+          : // TODO : 별도의 UI 처리 필요
+            ''}
     </p>
   );
 }
