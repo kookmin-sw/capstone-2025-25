a71f19824db6520fcf9800af152916df990fd212
ryugeon <sksmsfbrjs@naver.com>
2025-05-07 17:37:02 +0900

    feat: BubbleRepository 추가

--- backend/src/main/java/capstone/backend/domain/bubble/repository/BubbleRepository.java
+++ backend/src/main/java/capstone/backend/domain/bubble/repository/BubbleRepository.java
@@ -5,7 +5,9 @@ import capstone.backend.domain.bubble.entity.Bubble;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BubbleRepository extends JpaRepository<Bubble, Long> {
     List<Bubble> findAllByMemberId(Long memberId);
+    Optional<Bubble> findByMemberIdAndId(Long memberId, Long id);
 }
