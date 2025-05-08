ac7d120e87697c3488824a361531d1f5b777c684
judyzero <qozmcll@kookmin.ac.kr>
2025-05-08 11:20:12 +0900

    feat: 오늘의 할 일 추가 Controller 구현

---
+++ backend/src/main/java/capstone/backend/domain/todayTask/controller/TodayTaskItemController.java
@@ -0,0 +1,39 @@
+package capstone.backend.domain.todayTask.controller;
+
+import capstone.backend.domain.todayTask.dto.request.TodayTaskItemCreateRequest;
+import capstone.backend.domain.todayTask.dto.response.TodayTaskItemResponse;
+import capstone.backend.domain.todayTask.service.TodayTaskItemService;
+import capstone.backend.global.api.dto.ApiResponse;
+import capstone.backend.global.security.oauth2.user.CustomOAuth2User;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import java.time.LocalDate;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v2/today-task")
+@Tag(name="오늘의 할 일", description = "오늘의 할 일 관련 API")
+public class TodayTaskItemController {
+    private final TodayTaskItemService todayTaskItemService;
+
+    @Operation(summary = "오늘의 할 일 추가", description = "여러 개의 아이젠하워 할 일을 오늘의 할 일 목록에 추가")
+    @PostMapping
+    public ApiResponse<List<TodayTaskItemResponse>> addTodayTask(
+        @RequestBody @Valid TodayTaskItemCreateRequest request,
+        @AuthenticationPrincipal CustomOAuth2User user
+    ) {
+        List<TodayTaskItemResponse> response = todayTaskItemService.addTaskItem(user.getMemberId(), request);
+        return ApiResponse.ok(response);
+    }
+}
