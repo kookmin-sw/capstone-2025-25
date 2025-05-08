0b55ea54e42192f9bfd9c8526d7cae4238e04272
judyzero <qozmcll@kookmin.ac.kr>
2025-05-08 19:32:45 +0900

    feat: InventoryFolder 응답 DTO

---
+++ backend/src/main/java/capstone/backend/domain/inventory/response/InventoryFolderResponse.java
@@ -0,0 +1,15 @@
+package capstone.backend.domain.inventory.response;
+
+import capstone.backend.domain.inventory.entity.InventoryFolder;
+
+public record InventoryFolderResponse(
+    Long id,
+    String name
+) {
+    public static InventoryFolderResponse from(InventoryFolder inventoryFolder) {
+        return new InventoryFolderResponse(
+            inventoryFolder.getId(),
+            inventoryFolder.getName()
+        );
+    }
+}
