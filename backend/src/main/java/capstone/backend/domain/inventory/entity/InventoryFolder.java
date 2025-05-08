8063971824f4071777f47b18b9bca85bc731e629
judyzero <qozmcll@kookmin.ac.kr>
2025-05-08 19:30:16 +0900

    feat: InventoryFolder 엔티티 추가

---
+++ backend/src/main/java/capstone/backend/domain/inventory/entity/InventoryFolder.java
@@ -0,0 +1,38 @@
+package capstone.backend.domain.inventory.entity;
+
+
+import static lombok.AccessLevel.*;
+
+import capstone.backend.domain.inventory.request.InventoryFolderCreateRequest;
+import capstone.backend.domain.member.scheme.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PRIVATE)
+@Builder
+public class InventoryFolder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inventory_folder_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 10)
+    private String name;
+
+    public static InventoryFolder from(Member member, InventoryFolderCreateRequest inventoryFolderCreateRequest) {
+        return InventoryFolder.builder()
+            .member(member)
+            .name(inventoryFolderCreateRequest.name())
+            .build();
+    }
+}
