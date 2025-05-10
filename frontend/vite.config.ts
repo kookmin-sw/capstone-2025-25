8c1eb436ec130ff33d65d25fbbefee65e9b98eca
cychann <skcbdlhc129@naver.com>
2025-05-10 14:58:37 +0900

    [feat]: Vite PWA 라이브러리 설치 및 적용

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,15 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import { reactRouter } from '@react-router/dev/vite';
 import svgr from 'vite-plugin-svgr';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  plugins: [reactRouter(), tailwindcss(), svgr()],
+  plugins: [
+    reactRouter(),
+    tailwindcss(),
+    svgr(),
+    VitePWA({ registerType: 'autoUpdate' }),
+  ],
   server: {
     port: 3000,
   },
