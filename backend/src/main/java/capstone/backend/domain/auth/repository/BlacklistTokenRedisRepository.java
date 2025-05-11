aa6d62d396a84d2930d3dfdc2307949f880e701d
ryugeon <sksmsfbrjs@naver.com>
2025-05-12 01:55:42 +0900

    remove: 필요 없는 메서드 삭제

--- backend/src/main/java/capstone/backend/domain/auth/repository/BlacklistTokenRedisRepository.java
+++ backend/src/main/java/capstone/backend/domain/auth/repository/BlacklistTokenRedisRepository.java
@@ -4,5 +4,4 @@ import capstone.backend.domain.auth.schema.BlacklistToken;
 import org.springframework.data.keyvalue.repository.KeyValueRepository;
 
 public interface BlacklistTokenRedisRepository extends KeyValueRepository<BlacklistToken, String> {
-    boolean existsByToken(String token);
 }
