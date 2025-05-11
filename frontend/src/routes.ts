2cc920e40b1ff9aeb3fae822a2ba15ea756dba17
cychann <skcbdlhc129@naver.com>
2025-05-11 15:50:36 +0900

    [refactor]: 보관함 페이지명 수정
    - store -> inventory
    - 백엔드와 서비스 관련 네이밍을 통일

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -20,8 +20,8 @@ export default [
     route('pomodoro/:id?', 'pages/pomodoro.tsx'),
     route('list', 'pages/list.tsx'),
     route('dashboard', 'pages/dashboard.tsx'),
-    route('store', 'pages/store.tsx'),
-    route('store/:id', 'pages/storeDetail.tsx'),
+    route('inventory', 'pages/inventory.tsx'),
+    route('inventory/:id', 'pages/inventoryDetail.tsx'),
     route('brainstorming', 'pages/brainstorming.tsx'),
   ]),
 ] satisfies RouteConfig;
