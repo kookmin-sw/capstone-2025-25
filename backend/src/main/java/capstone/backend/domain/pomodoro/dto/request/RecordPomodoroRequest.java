a1117309cfd2aabd25bf4c94c425d0c7197fd55b
ryugeon <sksmsfbrjs@naver.com>
2025-05-12 14:31:01 +0900

    feat: DTO 단에서 유효성 검사 처리 (시간을 음수로 입력하거나 Null인 경우 방지)

--- backend/src/main/java/capstone/backend/domain/pomodoro/dto/request/RecordPomodoroRequest.java
+++ backend/src/main/java/capstone/backend/domain/pomodoro/dto/request/RecordPomodoroRequest.java
@@ -2,11 +2,13 @@ package capstone.backend.domain.pomodoro.dto.request;
 
 import capstone.backend.domain.pomodoro.schema.PomodoroCycle;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
 public record RecordPomodoroRequest(
+        @Valid
         @NotEmpty(message = "실제 실행 기록을 입력해주세요.")
         @Schema(description = "실제 실행 기간 별 초 단위 기록")
         List<PomodoroCycle> executedCycles
--- backend/src/main/java/capstone/backend/domain/pomodoro/schema/PomodoroCycle.java
+++ backend/src/main/java/capstone/backend/domain/pomodoro/schema/PomodoroCycle.java
@@ -1,12 +1,19 @@
 package capstone.backend.domain.pomodoro.schema;
 
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PomodoroCycle {
+    @NotNull(message = "작업 시간(workDuration)은 필수입니다.")
+    @Min(value = 0, message = "작업 시간(workDuration)은 0 이상이어야 합니다.")
     private Integer workDuration; // 초단위
+
+    @NotNull(message = "휴식 시간(breakDuration)은 필수입니다.")
+    @Min(value = 0, message = "휴식 시간(breakDuration)은 0 이상이어야 합니다.")
     private Integer breakDuration; // 초단위
 }
