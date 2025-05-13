a78e8ac8e1f638097245a24384f3be94f962fd6e
daun-up <awhekdns@kookmin.ac.kr>
2025-05-14 03:26:44 +0900

    fix: 로그인 페이지 접근 제한 해결

--- frontend/src/layouts/AuthLayout.tsx
+++ frontend/src/layouts/AuthLayout.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router';
+import { useAuthRedirect } from '@/hooks/useAuthRedirect.ts';
 
 export default function AuthLayout() {
+  useAuthRedirect();
   return (
     <div>
       {/*<header>Auth header</header>*/}
