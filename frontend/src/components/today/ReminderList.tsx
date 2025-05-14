f832bf84e9709c561647f17f7855b7e8c7be21e7
cychann <skcbdlhc129@naver.com>
2025-05-15 07:26:56 +0900

    [fix]: 라미인더 카드 제목 & 메모 한 줄까지만 보이도록

--- frontend/src/components/today/ReminderList.tsx
+++ frontend/src/components/today/ReminderList.tsx
@@ -20,9 +20,7 @@ export default function ReminderList() {
   return (
     <div className="w-full mb-6 flex flex-col gap-2 md:gap-[17px] ">
       <div className="flex items-center gap-4 mb-2 ml-2  md:ml-2">
-        <h4 className="text-[20px] text-[#525463] font-semibold">
-          리마인더
-        </h4>
+        <h4 className="text-[20px] text-[#525463] font-semibold">리마인더</h4>
         <p
           className="text-blue text-[14px] cursor-pointer"
           onClick={handleRouteToInventory}
@@ -41,10 +39,10 @@ export default function ReminderList() {
                 style={{ width: '16rem' }}
                 onClick={() => handleClickReminder(remind.folderId, remind.id)}
               >
-                <h3 className="text-[16px] md:text-[20px] text-[#525463] font-semibold">
+                <h3 className="text-[16px] md:text-[20px] text-[#525463] font-semibold whitespace-nowrap overflow-hidden text-ellipsis">
                   {remind.title}
                 </h3>
-                <p className="text-[14px] md:text-[16px] text-gray-scale-500">
+                <p className="text-[14px] md:text-[16px] text-gray-scale-500 whitespace-nowrap overflow-hidden text-ellipsis">
                   {remind.memo || '입력된 메모가 없습니다'}
                 </p>
               </div>
